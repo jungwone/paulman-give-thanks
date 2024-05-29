d0e19ef825c9e806f800a3312a05acb6184a3e1d
jungwone <prsjb77@gmail.com>
2024-05-29 22:21:11 +0900

    css & font 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata = {
 
 const ibmPlexFont = IBM_Plex_Sans_KR({
   subsets: ["latin"],
-  weight: ["300", "500", "700"],
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 export default function RootLayout({
